c000e8d181bec2cba59a8d7f032d1a8bfa063f38
Hamza Benfaddoul <hbenfadd@e1r4p12.1337.ma>
2023-10-25 10:03:22 +0100

    add main.h

---
+++ includes/main.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbenfadd <hbenfadd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 10:01:17 by hbenfadd          #+#    #+#             */
+/*   Updated: 2023/10/25 10:02:49 by hbenfadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+
+// c++ headers
+#include <iostream>
+#include <cstring>
+
+
+// c headers
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
