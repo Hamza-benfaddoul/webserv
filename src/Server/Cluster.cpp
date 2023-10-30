5b0f03e892811bd4f982f6dd86fa87d3c3c93677
rakhsas <rakhsas@rakhsas.42.fr>
2023-10-30 13:19:12 -0400

    send ip address from conf file

--- src/Server/Cluster.cpp
+++ src/Server/Cluster.cpp
@@ -21,7 +21,7 @@ Cluster::Cluster( std::vector<serverBlock> serverBlocks)
     // create a servers
     for (std::vector<serverBlock>::iterator it = serverBlocks.begin() + 1; it != serverBlocks.end(); ++it)
     {
-        servers.push_back(new Server(inet_addr(it->getHost().c_str()), it->getPort()));
+        servers.push_back(new Server(it->getHost(), it->getPort()));
     }
     // run all servers
     run();
