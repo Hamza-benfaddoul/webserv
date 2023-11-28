4657324fbc27c5ebecb4f96ab8a085ccc6ff0716
walid oumechtak <woumechtak@gmail.com>
2023-11-28 15:06:20 +0100

    update the timeout condition

--- src/Client/Upload.cpp
+++ src/Client/Upload.cpp
@@ -165,7 +165,7 @@ bool Upload::start()
 				end = clock();
 				// std::cout << "end: " << ((double)(end)) / CLOCKS_PER_SEC << std::endl;
 				// std::cout << ((double)(end - start_c)) / CLOCKS_PER_SEC << std::endl;
-				if (((double)(end - start_c)) / CLOCKS_PER_SEC > 20.0)
+				if (((double)(end - start_c)) / CLOCKS_PER_SEC > 60.0)
 				{
 					// send respone time out !!!!!
 					kill(pid, SIGKILL);
