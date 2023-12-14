0b8b08d6157b094ebd8a7ed6dfa79f15791a1fe8
rakhsas <akhsas36@gmail.com>
2023-12-14 22:58:09 +0100

    finished

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -223,7 +223,7 @@ bool Client::handleDirs()
 	{
 		if (location.index.length() > 0 && location.hasIndex == true)
 		{
-			
+
 			std::stringstream index(location.index);
 			std::string iter;
 			while (getline(index, iter, ','))
@@ -231,7 +231,7 @@ bool Client::handleDirs()
 				iter = advanced_trim(iter, " ");
 				struct dirent *pDirent;
 				DIR *pDir;
-				pDir = opendir((location.getRoot() + "/" + location.directory).c_str());
+				pDir = opendir((location.getRoot() + location.directory).c_str());
 				if (pDir == NULL)
 				{
 					std::cout << "Cannot open directory\n";
@@ -242,7 +242,7 @@ bool Client::handleDirs()
 				{
 					if (strcmp(iter.c_str(), pDirent->d_name) == 0)
 					{
-						handleFiles(location.getRoot() + "/" + location.directory + "/" + iter);
+						handleFiles(location.getRoot() + location.directory + "/" + iter);
 						return true;
 					}
 					fileCount++;
@@ -261,7 +261,7 @@ bool Client::handleDirs()
 		}
 		else if (location.getAutoIndex() == true)
 		{
-			directoryListing(location.getRoot() + location.directory);
+			directoryListing(location.getRoot() + requestedPath);
 		}
 	}
 	return true;
@@ -529,12 +529,12 @@ void Client::readFromCgi()
 bool Client::serveImage(std::string &headers)
 {
 	bool endOfFile = false;
-    if (_fdFile > -1)
-    {
-        size_t chunkSize = 1024;
-        char buffer[chunkSize];
-        size_t bytesRead = read(_fdFile, buffer, chunkSize);
-        if (bytesRead > 0)
+	if (_fdFile > -1)
+	{
+		size_t chunkSize = 1024;
+		char buffer[chunkSize];
+		size_t bytesRead = read(_fdFile, buffer, chunkSize);
+		if (bytesRead > 0)
 		{
 			std::stringstream chunkHeader;
 			chunkHeader << std::hex << bytesRead << "\r\n";
@@ -558,10 +558,10 @@ bool Client::serveImage(std::string &headers)
 			_fdFile = -1;
 			endOfFile = true;
 		}
-    }
+	}
 	write(_fd, headers.c_str(), headers.length());
 	fsync(_fd);
-    return endOfFile;
+	return endOfFile;
 }
 std::string Client::getErrorPage(int errorCode)
 {
@@ -580,16 +580,12 @@ bool Client::getMethodHandler(void)
 	std::string requestedPath = this->request->getPath();
 	size_t queryStringPos = requestedPath.find('?');
 	std::string filePath = (queryStringPos != std::string::npos) ? requestedPath.substr(0, queryStringPos) : requestedPath;
-	// std::cout << location.getRoot() + filePath << std::endl;
-	std::cout << location.getRoot() << std::endl;
-	std::cout << filePath << std::endl;
 	if (access((location.getRoot() + filePath).c_str(), R_OK) == -1)
 	{
 		sendErrorResponse(404, "Not Found", getErrorPage(404), _fd);
 	}
 	else if (checkType() == true)
 	{
-		std::cout << "handleDirs" << std::endl;
 		return handleDirs();
 	}
 	else if (checkType() == false)
@@ -829,114 +825,111 @@ void Client::directoryListing(std::string path)
 	headers << "Content-Type: text/html\r\n";
 	headers << "Content-Length: " << html.length() << "\r\n";
 	headers << "Connection: close\r\n\r\n";
-
-	// Write headers to socket
+	headers << html;
 	write(_fd, headers.str().c_str(), headers.str().length());
-
-	// Write HTML content to socket
-	write(_fd, html.c_str(), html.length());
 }
 
 std::string Client::generateDirectoryListing(const std::string& directoryPath)
 {
-    std::stringstream html;
-    html << "<style> h2 {"
-         << "color: #aaa;"
-         << "font-size: 30px;"
-         << "line-height: 40px;"
-         << "font-style: italic;"
-         << "font-weight: 200;"
-         << "margin: 40px;"
-         << "text-align: center;"
-         << "text-shadow: 1px 1px 1px rgba(255, 255, 255, 0.7);"
-         << "}"
-         << ".box {"
-         << "background: #fff;"
-         << "border-radius: 2px;"
-         << "box-shadow: 0 0 50px rgba(0, 0, 0, 0.1);"
-         << "margin: 30px 5%;"
-         << "padding: 5%;"
-         << "}"
-         << "@media (min-width: 544px) {"
-         << ".box {"
-         << "margin: 40px auto;"
-         << "max-width: 440px;"
-         << "padding: 40px;"
-         << "}"
-         << "}"
-         << ".directory-list ul {"
-         << "margin-left: 10px;"
-         << "padding-left: 20px;"
-         << "border-left: 1px dashed #ddd;"
-         << "}"
-         << ".directory-list li {"
-         << "list-style: none;"
-         << "color: #888;"
-         << "font-size: 17px;"
-         << "font-style: italic;"
-         << "font-weight: normal;"
-         << "}"
-         << ".directory-list a {"
-         << "border-bottom: 1px solid transparent;"
-         << "color: #888;"
-         << "text-decoration: none;"
-         << "transition: all 0.2s ease;"
-         << "}"
-         << ".directory-list a:hover {"
-         << "border-color: #eee;"
-         << "color: #000;"
-         << "}"
-         << ".directory-list .folder,"
-         << ".directory-list .folder > a {"
-         << "color: #777;"
-         << "font-weight: bold;"
-         << "}"
-         << ".directory-list li:before {"
-         << "margin-right: 10px;"
-         << "content: \"\";"
-         << "height: 20px;"
-         << "vertical-align: middle;"
-         << "width: 20px;"
-         << "background-repeat: no-repeat;"
-         << "display: inline-block;"
-         << "background-image: url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><path fill='lightgrey' d='M85.714,42.857V87.5c0,1.487-0.521,2.752-1.562,3.794c-1.042,1.041-2.308,1.562-3.795,1.562H19.643 c-1.488,0-2.753-0.521-3.794-1.562c-1.042-1.042-1.562-2.307-1.562-3.794v-75c0-1.487,0.521-2.752,1.562-3.794 c1.041-1.041,2.306-1.562,3.794-1.562H50V37.5c0,1.488,0.521,2.753,1.562,3.795s2.307,1.562,3.795,1.562H85.714z M85.546,35.714 H57.143V7.311c3.05,0.558,5.505,1.767,7.366,3.627l17.41,17.411C83.78,30.209,84.989,32.665,85.546,35.714z' /></svg>');background-position: center 2px;background-size: 60% auto;background-size: 60% auto;</style><body><h2>Directory List</h2><div class=\"box\"><ul class=\"directory-list\">";
+	std::stringstream html;
+	html << "<style> h2 {"
+		 << "color: #aaa;"
+		 << "font-size: 30px;"
+		 << "line-height: 40px;"
+		 << "font-style: italic;"
+		 << "font-weight: 200;"
+		 << "margin: 40px;"
+		 << "text-align: center;"
+		 << "text-shadow: 1px 1px 1px rgba(255, 255, 255, 0.7);"
+		 << "}"
+		 << ".box {"
+		 << "background: #fff;"
+		 << "border-radius: 2px;"
+		 << "box-shadow: 0 0 50px rgba(0, 0, 0, 0.1);"
+		 << "margin: 30px 5%;"
+		 << "padding: 5%;"
+		 << "}"
+		 << "@media (min-width: 544px) {"
+		 << ".box {"
+		 << "margin: 40px auto;"
+		 << "max-width: 440px;"
+		 << "padding: 40px;"
+		 << "}"
+		 << "}"
+		 << ".directory-list ul {"
+		 << "margin-left: 10px;"
+		 << "padding-left: 20px;"
+		 << "border-left: 1px dashed #ddd;"
+		 << "}"
+		 << ".directory-list li {"
+		 << "list-style: none;"
+		 << "color: #888;"
+		 << "font-size: 17px;"
+		 << "font-style: italic;"
+		 << "font-weight: normal;"
+		 << "}"
+		 << ".directory-list a {"
+		 << "border-bottom: 1px solid transparent;"
+		 << "color: #888;"
+		 << "text-decoration: none;"
+		 << "transition: all 0.2s ease;"
+		 << "}"
+		 << ".directory-list a:hover {"
+		 << "border-color: #eee;"
+		 << "color: #000;"
+		 << "}"
+		 << ".directory-list .folder,"
+		 << ".directory-list .folder > a {"
+		 << "color: #777;"
+		 << "font-weight: bold;"
+		 << "}"
+		 << ".directory-list li:before {"
+		 << "margin-right: 10px;"
+		 << "content: \"\";"
+		 << "height: 20px;"
+		 << "vertical-align: middle;"
+		 << "width: 20px;"
+		 << "background-repeat: no-repeat;"
+		 << "display: inline-block;"
+		 << "background-image: url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><path fill='lightgrey' d='M85.714,42.857V87.5c0,1.487-0.521,2.752-1.562,3.794c-1.042,1.041-2.308,1.562-3.795,1.562H19.643 c-1.488,0-2.753-0.521-3.794-1.562c-1.042-1.042-1.562-2.307-1.562-3.794v-75c0-1.487,0.521-2.752,1.562-3.794 c1.041-1.041,2.306-1.562,3.794-1.562H50V37.5c0,1.488,0.521,2.753,1.562,3.795s2.307,1.562,3.795,1.562H85.714z M85.546,35.714 H57.143V7.311c3.05,0.558,5.505,1.767,7.366,3.627l17.41,17.411C83.78,30.209,84.989,32.665,85.546,35.714z' /></svg>');background-position: center 2px;background-size: 60% auto;background-size: 60% auto;</style><body><h2>Directory List</h2><div class=\"box\"><ul class=\"directory-list\">";
 
-    DIR* dir;
-    struct dirent* entry;
-    struct stat entryStat;
-    // Open the directory
-    dir = opendir(directoryPath.c_str());
-    if (dir != NULL) {
-        // Read directory entries
-        while ((entry = readdir(dir)) != NULL) {
-            std::string name = entry->d_name;
-            // Skip current and parent directory entries
-            if (name != "." && name != "..") {
-                std::string fullPath = directoryPath + "/" + name;
-                stat(fullPath.c_str(), &entryStat);
-                // Check if the entry is a file or a directory
-                std::string href;
-                std::string listItemClass;
-                std::string iconSrc;
-                if (S_ISDIR(entryStat.st_mode)) {
-                    href =  location.directory + "/" + name + "/";
-                    listItemClass = "folder";
-                    iconSrc = "https://img.icons8.com/material-rounded/24/folder-invoices.png";
-                } else if(S_ISREG(entryStat.st_mode)) {
-                    href = location.directory + "/" + name;
-                    listItemClass = "file";
-                    iconSrc = "https://img.icons8.com/doodle/24/file--v1.png";
-                }
-                // Add an entry to the HTML list
-                html << "<li style='height: 40px !important;' class=\"" << listItemClass << "\"><a href=\"" << href << "\"><img width='24' height='24'  src=\"" << iconSrc << "\" alt=\"\" />" << name << "</a></li>";
-            }
-        }
-        closedir(dir);
-    } else {
-        // Handle directory open error
-        std::cerr << "Error opening directory: " << strerror(errno) << std::endl;
-    }
+	DIR* dir;
+	struct dirent* entry;
+	struct stat entryStat;
+	// Open the directory
+	std::cout << "directoryPath: " << directoryPath << std::endl;
+	dir = opendir(directoryPath.c_str());
+	if (dir != NULL) {
+		// Read directory entries
+		while ((entry = readdir(dir)) != NULL) {
+			std::string name = entry->d_name;
+			// Skip current and parent directory entries
+			if (name != "." && name != "..") {
+				std::string fullPath = directoryPath + "/" + name;
+				stat(fullPath.c_str(), &entryStat);
+				// Check if the entry is a file or a directory
+				std::string href;
+				std::string listItemClass;
+				std::string iconSrc;
+				if (S_ISDIR(entryStat.st_mode)) {
+					href = request->getPath() + name + "/";
+					listItemClass = "folder";
+					iconSrc = "https://img.icons8.com/material-rounded/24/folder-invoices.png";
+				} else if(S_ISREG(entryStat.st_mode)) {
+					href = request->getPath() + name;
+					listItemClass = "file";
+					iconSrc = "https://img.icons8.com/doodle/24/file--v1.png";
+				}
+				// Add an entry to the HTML list
+				html << "<li style='height: 40px !important;' class=\"" << listItemClass << "\"><a href=\"" << href << "\"><img width='24' height='24'  src=\"" << iconSrc << "\" alt=\"\" />" << name << "</a></li>";
+			}
+		}
+		closedir(dir);
+	} else {
+		// Handle directory open error
+		std::cerr << "Error opening directory: " << strerror(errno) << std::endl;
+	}
 
-    html << "</ul></div>";
-    return html.str();
+	html << "</ul></div>";
+	return html.str();
 }
